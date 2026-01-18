92e844d723a650ca96d25df6c9edad716e14c363
subeenhong <subeenyy@gmail.com>
2026-01-18 23:27:02 +0900

    add deleteCampaign

--- src/main/java/org/example/campaign/CampaignController.java
+++ src/main/java/org/example/campaign/CampaignController.java
@@ -20,109 +20,103 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CampaignController {
 
-    private final CampaignService campaignService;
-    private final JwtTokenProvider jwtTokenProvider;
+        private final CampaignService campaignService;
+        private final JwtTokenProvider jwtTokenProvider;
 
-    @GetMapping("/{campaignId}")
-    public CampaignResponseDto getCampaignById(
-            @PathVariable Long campaignId,
-            @RequestHeader("Authorization") String token
-    ) {
-        Long userId = extractUserId(token);
+        @GetMapping("/{campaignId}")
+        public CampaignResponseDto getCampaignById(
+                        @PathVariable Long campaignId,
+                        @RequestHeader("Authorization") String token) {
+                Long userId = extractUserId(token);
 
-        Campaign s = campaignService.findByCampaignIdAndUser(campaignId, userId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 건을 찾을 수 없습니다"));
+                Campaign s = campaignService.findByCampaignIdAndUser(campaignId, userId)
+                                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                                "해당 건을 찾을 수 없습니다"));
 
-        return CampaignResponseDto.fromEntity(s);
-    }
+                return CampaignResponseDto.fromEntity(s);
+        }
 
+        @PostMapping
+        public CampaignResponseDto createCampaign(
+                        @RequestHeader("Authorization") String token,
+                        @RequestBody @Valid CampaignCreateRequestDto sponsorship) {
+                Long userId = extractUserId(token);
+                Campaign campaign = campaignService.createCampaign(userId, sponsorship);
+                return CampaignResponseDto.fromEntity(campaign);
+        }
 
-    @PostMapping
-    public CampaignResponseDto createCampaign(
-            @RequestHeader("Authorization") String token,
-            @RequestBody @Valid CampaignCreateRequestDto sponsorship
-    ) {
-        Long userId = extractUserId(token);
-        Campaign campaign = campaignService.createCampaign(userId, sponsorship);
-        return CampaignResponseDto.fromEntity(campaign);
-    }
+        @GetMapping
+        public List<CampaignResponseDto> getCampaigns(
+                        @RequestHeader("Authorization") String token) {
+                Long userId = extractUserId(token);
+                return campaignService.findAllByUserId(userId)
+                                .stream()
+                                .map(CampaignResponseDto::fromEntity)
+                                .toList();
+        }
 
-    @GetMapping
-    public List<CampaignResponseDto> getCampaigns(
-            @RequestHeader("Authorization") String token
-    ) {
-        Long userId = extractUserId(token);
-        return campaignService.findAllByUserId(userId)
-                .stream()
-                .map(CampaignResponseDto::fromEntity)
-                .toList();
-    }
+        @PatchMapping("/{campaignId}")
+        public CampaignResponseDto updateCampaign(
+                        @PathVariable Long campaignId,
+                        @RequestHeader("Authorization") String token,
+                        @RequestBody CampaignResponseDto dto) {
+                Long userId = extractUserId(token);
 
-    
-    
-    
-    @PatchMapping("/{campaignId}")
-    public CampaignResponseDto updateCampaign(
-            @PathVariable Long campaignId,
-            @RequestHeader("Authorization") String token,
-            @RequestBody CampaignResponseDto dto
-    ) {
-        Long userId = extractUserId(token);
+                Campaign updated = campaignService.updateCampaign(campaignId, userId, dto);
+                return CampaignResponseDto.fromEntity(updated);
+        }
 
-        Campaign updated = campaignService.updateCampaign(campaignId, userId, dto);
-        return CampaignResponseDto.fromEntity(updated);
-    }
+        @DeleteMapping("/{campaignId}")
+        public void deleteCampaign(
+                        @PathVariable Long campaignId,
+                        @RequestHeader("Authorization") String token) {
+                Long userId = extractUserId(token);
+                campaignService.deleteCampaign(campaignId, userId);
+        }
 
+        @PatchMapping("/{campaignId}/status/{status}")
+        public void changeStatus(
+                        @PathVariable Long campaignId,
+                        @PathVariable CampaignAction status,
+                        @RequestHeader("Authorization") String token) {
+                Long userId = extractUserId(token);
+                campaignService.changeStatus(campaignId, userId, status);
+        }
 
-    @PatchMapping("/{campaignId}/status/{status}")
-    public void changeStatus(
-            @PathVariable Long campaignId,
-            @PathVariable CampaignAction status,
-            @RequestHeader("Authorization") String token
-    ) {
-        Long userId = extractUserId(token);
-        campaignService.changeStatus(campaignId, userId, status);
-    }
+        private Long extractUserId(String authHeader) {
 
-    private Long extractUserId(String authHeader) {
+                log.info("authHeader: [{}]", authHeader);
 
-        log.info("authHeader: [{}]", authHeader);
+                String token = authHeader.replace("Bearer ", "");
+                log.info("token: [{}]", token);
 
-        String token = authHeader.replace("Bearer ", "");
-        log.info("token: [{}]", token);
+                return jwtTokenProvider.getUserId(token);
 
-        return jwtTokenProvider.getUserId(token);
+        }
 
-    }
+        @PostMapping("/{campaignId}/review")
+        public ResponseEntity<String> submitReview(
+                        @PathVariable Long campaignId,
+                        @RequestHeader("Authorization") String token,
+                        @RequestParam String reviewUrl) {
 
-    @PostMapping("/{campaignId}/review")
-    public ResponseEntity<String> submitReview(
-            @PathVariable Long campaignId,
-            @RequestHeader("Authorization") String token,
-            @RequestParam String reviewUrl) {
+                Long userId = extractUserId(token);
+                campaignService.submitReview(campaignId, userId, reviewUrl);
+                return ResponseEntity.ok("Review submitted and reward triggered");
+        }
 
-        Long userId = extractUserId(token);
-        campaignService.submitReview(campaignId, userId, reviewUrl);
-        return ResponseEntity.ok("Review submitted and reward triggered");
-    }
+        @GetMapping("/status")
+        public List<CampaignResponseDto> getCampaignsByStatus(
+                        @RequestHeader("Authorization") String token,
+                        @RequestParam(required = false) Status status,
+                        @RequestParam(defaultValue = "asc") String order) {
+                Long userId = extractUserId(token);
 
-    @GetMapping("/status")
-    public List<CampaignResponseDto> getCampaignsByStatus(
-            @RequestHeader("Authorization") String token,
-            @RequestParam(required = false) Status status,
-            @RequestParam(defaultValue = "asc") String order
-    ) {
-        Long userId = extractUserId(token);
+                Sort sort = order.equalsIgnoreCase("desc")
+                                ? Sort.by("deadline").descending()
+                                : Sort.by("deadline").ascending();
 
-        Sort sort = order.equalsIgnoreCase("desc")
-                ? Sort.by("deadline").descending()
-                : Sort.by("deadline").ascending();
-
-        return campaignService.findCampaigns(userId, status, sort);
-    }
-
-
-
+                return campaignService.findCampaigns(userId, status, sort);
+        }
 
 }
-
--- src/main/java/org/example/campaign/CampaignService.java
+++ src/main/java/org/example/campaign/CampaignService.java
@@ -9,19 +9,15 @@ import org.example.user.User;
 import org.example.user.UserRepository;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
-
 
 @Slf4j
 @Service
@@ -34,15 +30,14 @@ public class CampaignService {
     private final UserRepository userRepository;
     private final CampaignMapper campaignMapper;
     private final KafkaTemplate<String, ReviewSubmittedEvent> kafkaTemplate;
-    private final RedisTemplate<String, Object> redisTemplate;
 
     public Campaign createCampaign(Long userId, CampaignCreateRequestDto request) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("유저 없음"));
 
         Platform platform = platformRepository.findById(request.getPlatformId())
-                .orElseThrow(()-> new IllegalArgumentException("플랫폼 없음"));
-        
+                .orElseThrow(() -> new IllegalArgumentException("플랫폼 없음"));
+
         boolean rewardEnabled = platform.isRewardEnabled();
         Long rewardPolicyId = platform.getRewardPolicyId();
 
@@ -51,16 +46,13 @@ public class CampaignService {
                 platform,
                 rewardEnabled,
                 rewardPolicyId,
-                request
-        );
+                request);
         return campaignRepository.save(campaign);
     }
-
 
     public List<Campaign> findAllByUserId(Long userId) {
         return campaignRepository.findByUserId(userId);
     }
-
 
     public void changeStatus(Long campaignId, Long userId, CampaignAction status) {
         Campaign s = campaignRepository.findByIdAndUser_Id(campaignId, userId)
@@ -68,7 +60,7 @@ public class CampaignService {
 
         switch (status) {
             case RESERVE -> s.reserve();
-            case VISIT  -> s.visit();
+            case VISIT -> s.visit();
             case COMPLETE -> s.complete();
             case CANCEL -> s.cancel();
             default -> throw new IllegalArgumentException("지원 상태로 되돌릴 수 없음");
@@ -85,12 +77,12 @@ public class CampaignService {
         return campaignRepository.save(campaign);
     }
 
-
     public Campaign updateCampaign(Long campaignId, Long userId, CampaignResponseDto dto) {
         Campaign existing = campaignRepository.findById(campaignId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
-        if(!existing.getUser().getId().equals(userId)) throw new ResponseStatusException(HttpStatus.FORBIDDEN, "본인 캠페인이 아닙니다.");
+        if (!existing.getUser().getId().equals(userId))
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "본인 캠페인이 아닙니다.");
 
         campaignMapper.updateFromDto(dto, existing);
 
@@ -111,33 +103,27 @@ public class CampaignService {
         campaign.complete(); // DONE
         kafkaTemplate.send(
                 "review-submitted",
-                new ReviewSubmittedEvent(campaign.getId(), userId, reviewUrl)
-        ).whenComplete((result, ex) -> {
-            if (ex != null) {
-                log.error("** Kafka 전송 실패", ex);
-            } else {
-                log.info(
-                        "** Kafka 전송 성공 topic={}, campaignId={}, userId={}, reviewUrl={}",
-                        result.getRecordMetadata().topic(),
-                        campaign.getId(),
-                        userId,
-                        reviewUrl
-                );
-            }
-        });
+                new ReviewSubmittedEvent(campaign.getId(), userId, reviewUrl)).whenComplete((result, ex) -> {
+                    if (ex != null) {
+                        log.error("** Kafka 전송 실패", ex);
+                    } else {
+                        log.info(
+                                "** Kafka 전송 성공 topic={}, campaignId={}, userId={}, reviewUrl={}",
+                                result.getRecordMetadata().topic(),
+                                campaign.getId(),
+                                userId,
+                                reviewUrl);
+                    }
+                });
 
     }
 
     @Transactional(readOnly = true)
-    @Cacheable(
-            value = "campaigns",
-            key = "'user:' + #userId + ':status:' + #status + ':order:' + #sort"
-    )
+    @Cacheable(value = "campaigns", key = "'user:' + #userId + ':status:' + #status + ':order:' + #sort")
     public List<CampaignResponseDto> findCampaigns(
             Long userId,
             Status status,
-            Sort sort
-    ) {
+            Sort sort) {
         List<Campaign> campaigns;
 
         if (status == null) {
@@ -151,5 +137,16 @@ public class CampaignService {
                 .toList();
     }
 
+    @org.springframework.cache.annotation.CacheEvict(value = "campaigns", allEntries = true)
+    public void deleteCampaign(Long campaignId, Long userId) {
+        Campaign campaign = campaignRepository.findById(campaignId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "캠페인을 찾을 수 없습니다."));
+
+        if (!campaign.getUser().getId().equals(userId)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "본인의 캠페인만 삭제할 수 있습니다.");
+        }
+
+        campaignRepository.delete(campaign);
+    }
 
 }
