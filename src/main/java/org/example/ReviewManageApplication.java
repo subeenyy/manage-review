9d0ce7c3360fa74c05a6ad825ececfe3618df498
subeenhong <subeenyy@gmail.com>
2026-01-27 21:41:56 +0900

    Add startup logging and simplify JPA config for debugging

--- src/main/java/org/example/ReviewManageApplication.java
+++ src/main/java/org/example/ReviewManageApplication.java
@@ -4,14 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableScheduling
 @EntityScan("org.example")
 @SpringBootApplication
+@EnableJpaAuditing
 public class ReviewManageApplication {
     public static void main(String[] args) {
+        System.out.println(">>> STARTING APPLICATION...");
         SpringApplication.run(ReviewManageApplication.class, args);
+        System.out.println(">>> APPLICATION STARTED SUCCESSFULLY");
     }
 }
